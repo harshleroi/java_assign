73aebebbf523ae757842ba79e94c5e45e27e4f72
harshleroi <impwork8053@gmail.com>
2022-02-17 14:17:09 +0530

    .

--- src/java_assignment/test_class.java
+++ src/java_assignment/test_class.java
@@ -4,11 +4,9 @@ public class test_class {
 	public class TestCircle {     
 		   public static void main(String[] args) {
 		      Circle c1 = new Circle();
-		      System.out.println("The circle has radius of " 
-		         + c1.getRadius() + " and area of " + c1.getArea());		   
+		      System.out.println( "radius = " + c1.getRadius() + " and area is = " + c1.getArea());		   
 		      Circle c2 = new Circle(2.0);
-		      System.out.println("The circle has radius of " 
-		         + c2.getRadius() + " and area of " + c2.getArea());
+		      System.out.println( "radius = " + c2.getRadius() + " and area is = " + c2.getArea()); 
 		   }
 	}
 }
