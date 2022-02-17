1cd3a3f68b6aab364f59ae818d3d614bc26ecc2d
harshleroi <impwork8053@gmail.com>
2022-02-17 12:56:18 +0530

    update

--- src/java_assignment/Circle.java
+++ src/java_assignment/Circle.java
@@ -4,19 +4,17 @@ public class Circle {
 	 private double radius;
 	   private String color;
 	   
-	   // 1st constructor, which sets both radius and color to default
+	  
 	   public Circle() {
 	      radius = 1.0;
 	      color = "red";
 	   }
 	   
-	   // 2nd constructor with given radius, but color default
 	   public Circle(double r) {
 	      radius = r;
 	      color = "red";
 	   }
 	   
-	   // A public method for retrieving the radius
 	   public double getRadius() {
 	     return radius; 
 	   }
